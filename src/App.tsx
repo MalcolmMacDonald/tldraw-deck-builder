4d5cb3f282a3fbaf177f1599b1dbdfc656364053
Malcolmngx <mmacdonald@ngxinteractive.com>
2025-06-02 17:12:45 -0700

    Added code to drive initial state from json that sits in github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,49 @@
+import {Editor, Tldraw, TLStoreSnapshot,} from 'tldraw'
+import {CustomMainMenu} from '@/CustomMainMenu'
 import {
-	Editor, loadSnapshot,
-	Tldraw, TLSessionStateSnapshot,
-	TLStoreSnapshot,
-} from 'tldraw'
-import { CustomMainMenu } from '@/CustomMainMenu'
-import { ClickPropagator, ChangePropagator, TickPropagator, SpatialPropagator, registerPropagators } from '@/propagators/ScopedPropagators'
-import initialSnapshot  from '@/InitialState.json';
+    ChangePropagator,
+    ClickPropagator,
+    registerPropagators,
+    SpatialPropagator,
+    TickPropagator
+} from '@/propagators/ScopedPropagators'
+import React from "react";
+
+const initialSnapshotURL = './InitialState.json';
+
 export default function YjsExample() {
-	return (
-		<div className="tldraw__editor">
-			<Tldraw
-				components={{
-					MainMenu: CustomMainMenu,
-				}}
-				onMount={onMount}
-				snapshot={initialSnapshot.document as TLStoreSnapshot}
-				persistenceKey='async-async-scoped-propagators'
-			/>
-		</div>
-	)
+    //fetch  the initial snapshot from the JSON file
+    const [initialSnapshot, setInitialSnapshot] = React.useState<TLStoreSnapshot | null>(null)
+    React.useEffect(() => {
+        fetch(initialSnapshotURL)
+            .then(response => response.json())
+            .then(data => setInitialSnapshot(data as TLStoreSnapshot))
+            .catch(error => console.error('Error fetching initial snapshot:', error));
+    }, []);
+    return (
+        <div className="tldraw__editor">
+            <Tldraw
+                components={{
+                    MainMenu: CustomMainMenu,
+                }}
+                onMount={onMount}
+                snapshot={initialSnapshot as TLStoreSnapshot}
+                persistenceKey='async-async-scoped-propagators'
+            />
+        </div>
+    )
 }
 
- function onMount(editor: Editor) {
-	//@ts-expect-error
-	window.editor = editor
-	// stop double click text creation
- 	// @ts-expect-error
-	editor.getStateDescendant('select.idle').handleDoubleClickOnCanvas = () => void null;
-	registerPropagators(editor, [
-		ChangePropagator,
-		ClickPropagator,
-		TickPropagator,
-		SpatialPropagator,
-	])
+function onMount(editor: Editor) {
+    //@ts-expect-error
+    window.editor = editor
+    // stop double click text creation
+    // @ts-expect-error
+    editor.getStateDescendant('select.idle').handleDoubleClickOnCanvas = () => void null;
+    registerPropagators(editor, [
+        ChangePropagator,
+        ClickPropagator,
+        TickPropagator,
+        SpatialPropagator,
+    ])
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 export default defineConfig({
-  base: '/async-scoped-propagators/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+    base: '/async-scoped-propagators/',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    build: {
+        rollupOptions: {
+            external: [
+                'src/InitialState.json',
+            ]
+        }
     }
-  }
 })
